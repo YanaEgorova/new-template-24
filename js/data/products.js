72150cde86c6dc99530227244e2c02f4b71c2951
Yana <yanaegorova69.3@gmail.com>
2021-09-16 02:09:42 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,154 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Photo Picture Frame',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Simple, classic photo frames available in silver or black.',
+            },
+            {
+                text: 'Display art and photo prints of friends, family, vacations, and more.',
+            },
+            {
+                text: 'Includes an easel-back for displaying on a shelf or table top as well as hooks for hanging on the wall.',
+            },
+            {
+                text: 'Can be hung horizontally or vertically.',
+            },
+            {
+                text: 'Comes with a glass face that will protect prints from wear and damage.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Bath Shelf',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Teak Wood.',
+            },
+            {
+                text: 'Made from natural Teak.',
+            },
+            {
+                text: 'Smooth finish prevents water stains.',
+            },
+            {
+                text: 'Conveniently store your soap, cell phone, snack, or drink.',
+            },
+            {
+                text: 'Dimensions: 33. 5"W x 8"L x 2. 5"H',
+            },
+            {
+                text: 'No Assembly Required.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'PCP Shower Safety Chair',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'PCP 7107 portable shower support; use for injury recovery, patient transfer and bathing.',
+            },
+            {
+                text: 'Pivoting armrests are secured in the down position by a push-release catch for greater security.',
+            },
+            {
+                text: 'Easy lateral transfers; non-marring, non-slip suction tips; best used for elderly, injured, disabled.',
+            },
+            {
+                text: 'Lightweight medical grade spa aid; anodized frame prevents rust; easy assembly.',
+            },
+            {
+                text: 'Seat height adjustment: 16" - 20"; Seat dimensions: 16" X 13"; base dimensions: 10" X 16.25"',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png', './img/img3-5.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Bath Bundle',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Included Newborn Support gives additional comfort for your baby or infant.',
+            },
+            {
+                text: 'Suitable for newborn babies and children up to the age of four.',
+            },
+            {
+                text: 'A lightweight, portable baby bath tub that folds flat.',
+            },
+            {
+                text: 'The bath tub has a non-slip base and a plug for easy draining.',
+            },
+            {
+                text: 'Award-winning and unique space saving design.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Bean Bag Chair',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'A Touch Of Personality',
+                text: 'Easily customize your living room or bedroom with this bold eggplant bean bag. With an unbeatable combination of soft textures and impeccable style, this accessory will be sure to enliven any lounge space.',
+            },
+            {
+                title: 'Modern Design',
+                text: 'With clean, precise double-stitched lines, our bean bag is the ideal modern accessory for your interior space. Finished with a playful and bold design, this accessory not only offers an outstanding minimalistic look but also provides incredible comfort.',
+            },
+            {
+                title: 'Removable Covers With Safety Zippers',
+                text: 'You can easily remove the bean bag cover for cleaning purposes after a long period of use. Just use the childproof safety zippers to unfasten the skin and slip out the inner filling. We recommend spot cleaning this item or having it dry cleaned to extend the lifespan of this product.',
+            },
+            {
+                title: 'Eco-Friendly Foam Filling',
+                text: 'Our super soft bean bags are filled with small chunks of high-density foam, which helps retain shape and provides amazing support. With its durable nature, this foam is made to last longer and softer. The filling comes in an inner liner, so you don\'t have to worry about any mess.',
+            },
+            {
+                title: 'Dimensions',
+                text: 'Choose an accessory that is the perfect size for you and your furniture. This bean bag measures to be 94.00” L x 44.00” D x 20.00” H. You will love how much your space can transform with the simple addition of this charming bean bag.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.png', './img/img5-5.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Twin Push Sweeper',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Cleans up to 5X quicker than a conventional broom.',
+            },
+            {
+                text: 'Sweeps up to 26,000 square feet per hour.',
+            },
+            {
+                text: 'Human-powered sweeper mechanism requires no electricity.',
+            },
+            {
+                text: 'Easy to push with minimal physical effort, weighs only 22 pounds.',
+            },
+            {
+                text: 'Two-stage height adjustment for personal preference.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-3.png', './img/img6-4.png', './img/img6-5.png'],
+    },
+]
